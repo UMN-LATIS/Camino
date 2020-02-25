fa1f6b88bae19ce5013e861bae5a1d61e20348d8
Colin McFadden <cmcfadden@gmail.com>
2020-02-25 11:10:29 -0600

    remove altitudes

--- resources/js/components/ARembed.vue
+++ resources/js/components/ARembed.vue
@@ -3,21 +3,21 @@
       <a-scene  v-pre vr-mode-ui="enabled: false" 
                     arjs='sourceType: webcam; sourceWidth:790; sourceHeight:670; displayWidth:790; displayHeight:670; debugUIEnabled: false;'>
              
-        <a-text value="Elliot Hall" gps-entity-place="latitude: 44.97691; longitude: -93.23776;" position="0 100 0"
+        <a-text value="Elliot Hall" gps-entity-place="latitude: 44.97691; longitude: -93.23776;"
             rotation="0 0 0" font="mozillavr" color="#e43e31" look-at="#camera" side="double" align="center"
             width="400">
         </a-text>
-                <a-text value="Weisman" gps-entity-place="latitude: 44.97310; longitude: -93.23783;" position="0 200 0"
+                <a-text value="Weisman" gps-entity-place="latitude: 44.97310; longitude: -93.23783;"
                     rotation="0 0 0" font="mozillavr" color="#e43e31" look-at="#camera" side="double" align="center"
                     width="400">
                 </a-text>
 
-        <a-text value="Andersen Library" gps-entity-place="latitude: 44.97322; longitude: -93.24298;" position="0 100 0"
+        <a-text value="Andersen Library" gps-entity-place="latitude: 44.97322; longitude: -93.24298;"
             rotation="0 0 0" font="mozillavr" color="#e43e31" look-at="#camera" side="double" align="center"
             width="200">
         </a-text>
     <a-camera id="camera"
-            gps-camera="simulateLatitude: 44.971958; simulateLongitude: -93.2422704; simulateAltitude: 300"
+            gps-camera
             rotation-reader far=900000>
         </a-camera>
                 </a-scene>

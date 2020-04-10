4d3c829988b28b131588907d7660971dc0ceff9a
Colin McFadden <cmcfadden@gmail.com>
2020-04-10 18:59:53 -0500

    misc

--- resources/js/components/edit/TourStop.vue
+++ resources/js/components/edit/TourStop.vue
@@ -22,18 +22,6 @@ export default {
     props: ["stop", "languages"],
     data() {
         return {
-            // "title": {},
-            // "location":{},
-            // "stages":[]
-        }
-    },
-    methods: {
-        updated: function() {
-            // var stop = {};
-            // stop.title = this.title;
-            // stop.location = this.location;
-            // stop.stages = this.stages;
-            // this.$emit('update:stop', stop);
         }
     },
     created: function() {

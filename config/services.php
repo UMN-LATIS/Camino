f5c15c1eb244ad36d87f7ad99ea25b5cbedfc5dd
Colin McFadden <cmcfadden@gmail.com>
2020-05-07 16:22:23 -0500

    make services configurable

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8000/socialite/github/callback',
+        'redirect' => env('APP_URL') . '/socialite/github/callback',
     ],
 
 ];

6190afb518847b01a13fead84830d2526db9d183
Colin McFadden <cmcfadden@gmail.com>
2020-06-08 09:52:21 -0500

    allow multiple iconS

--- resources/js/components/TransportIcon.vue
+++ resources/js/components/TransportIcon.vue
@@ -1,7 +1,9 @@
 <template>
-    <i class="fas fa-walking" v-if="tour.walking==1"></i>
-    <i class="fas fa-biking" v-else-if="tour.biking==1"></i>
-    <i class="fas fa-car" v-else-if="tour.driving==1"></i>
+    <div>
+        <i class="fas fa-walking" v-if="tour.walking==1"></i>
+        <i class="fas fa-biking" v-if="tour.biking==1"></i>
+        <i class="fas fa-car" v-if="tour.driving==1"></i>
+    </div>
 </template>
 
 <script>

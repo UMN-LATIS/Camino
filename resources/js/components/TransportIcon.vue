dcca4f729f32b01d7fcb5b9b937f9563aa0c230a
Colin McFadden <cmcfadden@gmail.com>
2020-06-08 09:54:38 -0500

    make it a span

--- resources/js/components/TransportIcon.vue
+++ resources/js/components/TransportIcon.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <span>
         <i class="fas fa-walking" v-if="tour.walking==1"></i>
         <i class="fas fa-biking" v-if="tour.biking==1"></i>
         <i class="fas fa-car" v-if="tour.driving==1"></i>
-    </div>
+    </span>
 </template>
 
 <script>

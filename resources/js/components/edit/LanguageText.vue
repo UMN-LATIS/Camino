55a4e8a2752241ed5522d213d9b755c41445a983
James Johnson <johnsojr@umn.edu>
2022-02-09 22:14:33 -0600

    fix col alignment

--- resources/js/components/edit/LanguageText.vue
+++ resources/js/components/edit/LanguageText.vue
@@ -2,7 +2,7 @@
     <div>
         <template v-if="largetext">
              <div class="row">
-                 <div v-for="(language, key) in languages" :key="key" class="form-group col-sm-6">
+                 <div v-for="(language, key) in languages" :key="key" class="form-group col">
                      <label :for="'field' + key + randomIdentifier" class=""><slot/> ({{ language }})</label>
                     <custom-markdown v-if="largetext" :text.sync="text[language]" :idkey="'field' + key + randomIdentifier"></custom-markdown>
                 </div>
@@ -14,7 +14,7 @@
             <div v-for="(language, key) in languages" :key="key">
                 <div class="form-group row">
                     <label :for="'field' + key + randomIdentifier" class="col-sm-2 col-form-label"><slot/> ({{ language }})</label>
-                    <div class="col-sm-6">
+                    <div class="col-sm-10">
                         <input type="text" class="form-control" v-model="text[language]" placeholder="" :id="'field' + key + randomIdentifier">
                     </div>
                     <div class="col-sm-4">

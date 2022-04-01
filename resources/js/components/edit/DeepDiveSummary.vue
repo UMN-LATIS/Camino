b35ddf6ab8032c988e9e8b30571a34359bca3335
James Johnson <johnsojr@umn.edu>
2022-04-01 14:23:46 -0500

    Revert "Remove request email checkbox"
    This reverts commit cbea0ea62c7c1e46627d41191a3e84e70f2d47bc.

--- resources/js/components/edit/DeepDiveSummary.vue
+++ resources/js/components/edit/DeepDiveSummary.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
+    <div class="form-check">
+      <label class="form-check-label">
+        <!-- FIXME: This is mutating the stage prop! Ignoring for now. -->
+        <!-- eslint-disable -->
+        <input
+          type="checkbox"
+          class="form-check-input"
+          v-model="stage.request_email"
+        />
+        <!-- eslint-enable -->
+        Request Email Addresses
+      </label>
+    </div>
     <language-text :text="stage.text" :languages="languages" :largetext="true">
       Deep Dive Text
     </language-text>

b6f95c21101185816fb524cfb5f8a8d9e1b527a7
James Johnson <johnsojr@umn.edu>
2022-04-07 15:41:27 -0500

    update i18n

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -1,7 +1,4 @@
-import Vue from "vue";
-import VueI18n from "vue-i18n";
-
-Vue.use(VueI18n);
+import { createI18n } from "vue-i18n";
 
 function loadLocaleMessages() {
   const locales = require.context(
@@ -20,8 +17,9 @@ function loadLocaleMessages() {
   return messages;
 }
 
-export default new VueI18n({
+export default createI18n({
   locale: "en",
   fallbackLocale: "en",
+  allowComposition: true,
   messages: loadLocaleMessages(),
 });

e95cf890d031c352f85dd88ffe7673788552c265
James Johnson <johnsojr@umn.edu>
2022-04-13 11:34:04 -0500

    fix: Teleport cannot be root element of component

--- resources/js/tempComponents/BModal.vue
+++ resources/js/tempComponents/BModal.vue
@@ -1,29 +1,49 @@
 <template>
-  <Teleport to="body">
-    <div class="modal">
-      <div class="modal-inner">
-        <slot />
+  <div class="b-modal">
+    <Teleport to="body">
+      <div
+        :id="id"
+        class="modal"
+        tabindex="-1"
+        :aria-labelledby="`${id}-title`"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 :id="`${id}-title`" class="modal-title">{{ title }}</h5>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                @click="$emit('close')"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <slot />
+            </div>
+            <div class="modal-footer">
+              <slot name="footer" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </Teleport>
+    </Teleport>
+  </div>
 </template>
-<script setup></script>
-<style scoped>
-.modal {
-  display: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-}
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    default: "modal-id",
+  },
+  title: {
+    type: String,
+    default: "Modal Title",
+  },
+});
 
-.modal-inner {
-  max-width: 75vw;
-  max-height: 75vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+defineEmits(["close"]);
+</script>
+<style scoped></style>

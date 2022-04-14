92910138a82c0f20798521574c04ba49e16f7089
James Johnson <johnsojr@umn.edu>
2022-04-14 15:03:12 -0500

    fix language toggle

---
+++ resources/camino-creator/pages/EditPage/SelectTransport.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="form-group row my-4">
+    <label for="tourTitle" class="col-sm-2">Transport</label>
+    <div class="col-sm-6">
+      <div
+        v-for="(transport, index) in transportOptions"
+        :key="index"
+        class="form-check"
+      >
+        <label class="form-check-label">
+          <input
+            :checked="props[transport]"
+            type="checkbox"
+            class="form-check-input"
+            @change="$emit(`update:${transport}`, $event.target.checked)"
+          />
+          {{ capitalize(transport) }}
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { computed } from "@vue/reactivity";
+
+const props = defineProps({
+  walking: {
+    type: Boolean,
+    default: false,
+  },
+  biking: {
+    type: Boolean,
+    default: false,
+  },
+  driving: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const transportOptions = computed(() => Object.keys(props));
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+defineEmits(["update:walking", "update:biking", "update:driving"]);
+</script>

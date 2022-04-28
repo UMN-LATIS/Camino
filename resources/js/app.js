ff266ba7e506e1f325c301de29c7e9f5ddacda51
James Johnson <johnsojr@umn.edu>
2022-04-28 13:22:58 -0500

    change ar-embed to static import

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,13 @@ import initAxios from "../camino-creator/common/initAxios.js";
 import initLeaflet from "../camino-creator/common/initLeaflet.js";
 import { createApp } from "vue";
 import FindTourPage from "./FindTourPage.vue";
+import ARembed from "./components/ARembed.vue";
 
 initAxios();
 initLeaflet();
 
 const app = createApp({});
 app.component("find-tour-page", FindTourPage);
-app.component("ar-embed", () => import("./components/ARembed.vue"));
+app.component("ar-embed", ARembed);
 
 app.mount("#app");

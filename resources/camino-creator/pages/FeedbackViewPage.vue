f8fd55166993e00edd84c9a51c6dd03d9614f603
James Johnson <johnsojr@umn.edu>
2022-05-27 17:29:19 -0500

    use FeedbackResponse + some fixes (thanks ts!)

--- resources/camino-creator/pages/FeedbackViewPage.vue
+++ resources/camino-creator/pages/FeedbackViewPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Feedback</h1>
-    <p v-if="!feedback.lenght">No feedback yet.</p>
+    <p v-if="!feedback.length">No feedback yet.</p>
     <div
       v-for="feedbackResponse in feedback"
       :key="feedbackResponse.id"
@@ -9,7 +9,7 @@
     >
       <div class="card-body">
         <h4 class="card-title">
-          {{ feedbackResponse.name }} {{ feedback.email }}
+          {{ feedbackResponse.name }} {{ feedbackResponse.email }}
         </h4>
         <p class="card-text">{{ feedbackResponse.created_at }}</p>
         <p class="card-text">{{ feedbackResponse.feedback }}</p>
@@ -18,19 +18,22 @@
   </div>
 </template>
 
-<script>
-export default {
-  props: ["tourId"],
-  data() {
-    return {
-      feedback: [],
-    };
-  },
-  mounted() {
-    axios.get("/creator/" + this.tourId + "/feedback").then((res) => {
-      this.feedback = res.data;
-    });
-  },
-  methods: {},
-};
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { axiosClient as axios } from "@creator/common/axios";
+import type { FeedbackResponse } from "@/types";
+
+interface Props {
+  tourId: number;
+}
+
+const props = defineProps<Props>();
+
+const feedback = ref<FeedbackResponse[]>([]);
+
+onMounted(() => {
+  axios.get(`/creator/${props.tourId}/feedback`).then((res) => {
+    feedback.value = res.data;
+  });
+});
 </script>

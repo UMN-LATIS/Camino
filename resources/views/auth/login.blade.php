7191673ed74f73380d97c8c5b1618dbd267a7d41
James Johnson <johnsojr@umn.edu>
2022-05-31 11:46:49 -0500

    add margin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,28 +10,20 @@
                 <div class="card-header">Log In to Camino</div>
 
                 <div class="card-body">
-                      <div class="form-group row d-flex justify-content-center">
-                            <div class="col-12 col-md-12 col-lg-6 ">
-                                <a href="{{ url('/shiblogin') }}" class="btn university btn-lg d-flex align-items-center justify-content-center text-light"><i class="fa fa-university fa-2x fa-inverse  mr-2"></i> University of Minnesota</a>
+                      <div class="form-group row d-flex justify-content-center mb-4">
+                            <div class="col-12 col-md-12 col-lg-6">
+                                <a href="{{ url('/shiblogin') }}" class="btn university btn-lg d-flex align-items-center justify-content-center text-light"><i class="fa fa-university fa-2x fa-inverse me-2"></i> University of Minnesota</a>
                             </div>
                         </div>
-                         {{-- <div class="form-group row">
-                            <div class="col-md-6 offset-md-3 text-center">
-                                <p class="display-4">or</p>
-                            </div>
-                        </div> --}}
                         <div class="form-group row">
                             <div class="col-12  col-md-12 col-lg-4 ">
-                                <a href="{{ url('/socialite/google') }}" class="m-1 m-lg-0 btn google btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-google fa-2x fa-inverse  mr-2"></i> Google Login</a>
-                                <span class="hidden-xs"></span>
+                                <a href="{{ url('/socialite/google') }}" class="m-1 m-lg-0 btn google btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-google fa-2x fa-inverse me-2"></i> Google Login</a>
                             </div>
                             <div class="col-12  col-md-12 col-lg-4 ">
-                                <a href="{{ url('/socialite/live') }}" class="m-1 m-lg-0 btn github btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-microsoft fa-2x fa-inverse  mr-2"></i> Microsoft Login</a>
-                                <span class="hidden-xs"></span>
+                                <a href="{{ url('/socialite/live') }}" class="m-1 m-lg-0 btn github btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-microsoft fa-2x fa-inverse me-2"></i> Microsoft Login</a>
                             </div>
                              <div class="col-12  col-md-12 col-lg-4 ">
-                                <a href="{{ url('/socialite/facebook') }}" class="m-1 m-lg-0 btn facebook btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-facebook fa-2x fa-inverse mr-2"></i> Facebook Login</a>
-                                <span class="hidden-xs"></span>
+                                <a href="{{ url('/socialite/facebook') }}" class="m-1 m-lg-0 btn facebook btn-lg d-flex align-items-center justify-content-center text-light"><i class="fab fa-facebook fa-2x fa-inverse me-2"></i> Facebook Login</a>
                             </div>
                         </div>
                        

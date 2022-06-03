827a4a21e8f36aa3094027cd46878f265e36a1fe
James Johnson <johnsojr@umn.edu>
2022-06-03 15:48:28 -0500

    add empty subtitle field to any stops missing it

---
+++ database/migrations/2022_06_03_192302_add_subtitle_to_stop.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Stop;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stop', function (Blueprint $table) {
+            Stop::all()->each(function ($stop) {
+                if (isset($stop->stop_content['subtitle'])) {
+                    return;
+                }
+
+                // add an empty {} LocalizedText object
+                $stop->stop_content = [
+                    ...$stop->stop_content,
+                    'subtitle' => new ArrayObject([]),
+                ];
+                $stop->save();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stop', function (Blueprint $table) {
+            //
+        });
+    }
+};

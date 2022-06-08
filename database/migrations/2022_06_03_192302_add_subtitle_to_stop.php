7ae3a750513b77326bf8e2955bb37f27deaa3968
James Johnson <johnsojr@umn.edu>
2022-06-07 22:53:11 -0500

    Add migration for subtitle property in `stop_content` (#185)
    * add empty subtitle field to any stops missing it
    * update seeders to include empty subtitle field
    * change default to null

---
+++ database/migrations/2022_06_03_192302_add_subtitle_to_stop.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Stop;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stop', function () {
+            Stop::all()->each(function ($stop) {
+                if (array_key_exists('subtitle', $stop->stop_content)) {
+                    return;
+                }
+
+                $stop->stop_content['subtitle'] = NULL;
+                $stop->save();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stop', function (Blueprint $table) {
+            //
+        });
+    }
+};

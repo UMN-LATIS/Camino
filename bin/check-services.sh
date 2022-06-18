ee83ecbcd988a3490327d9ca798745b4c79400e9
James Johnson <johnsojr@umn.edu>
2022-06-17 22:06:03 -0500

    exit 0 explicitly

--- bin/check-services.sh
+++ bin/check-services.sh
@@ -5,5 +5,7 @@ docker compose ps --all
 
 if [[ $(docker compose ps -aq --filter status=exited) ]]; then 
   echo "Error: Some services are not running." 1>&2
-  exit 1;
+  exit 1; 
+else
+  exit 0;  
 fi

69b6a8754361d0beeca2da8cd9ddb1e2fc20564b
James Johnson <johnsojr@umn.edu>
2022-06-17 21:29:03 -0500

    move chown to ci.sh

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,6 +5,8 @@ php artisan key:generate
 
 # Directory Permissions
 php artisan storage:link
+chown -R 1000:1000 storage
+chown -R 1000:1000 bootstrap/cache
 chmod -R 777 ./storage
 chmod -R 777 ./bootstrap/cache
 php artisan config:clear

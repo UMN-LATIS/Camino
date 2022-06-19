fa13e8acb554d8425755a7a5a4ad8494973fbb3d
James Johnson <johnsojr@umn.edu>
2022-06-19 15:20:06 -0500

    test add tourstoppage change stop title

--- cypress/integration/creator/TourStopPage.spec.js
+++ cypress/integration/creator/TourStopPage.spec.js
@@ -1,6 +1,43 @@
 describe("Tour Stop Page", () => {
-  it("changes the stop name");
+  beforeEach(() => {
+    cy.refreshDatabase();
+    cy.seed();
+    cy.login({ unique_id: "admin" });
+    cy.visit("/creator");
+    cy.contains("New Tour").click();
+
+    // create a new test tour for editing
+    cy.get("#new-title").type("Test Tour{enter}");
+    cy.contains("Test Tour")
+      .closest(".tour-list-item")
+      .contains("Edit")
+      .click();
+
+    // create a new tour stop
+    cy.contains("New Stop").click();
+    cy.get('[data-cy="add-stop-form"] input').type("Test Stop{enter}");
+    cy.contains("Test Stop").click();
+  });
+
+  it("changes the stop name", () => {
+    cy.get('[data-cy="stop-title"]').should("contain.text", "Test Stop");
+    cy.get('[data-cy="stop-title-input-group"]').type("{selectAll}Stop 1");
+    cy.contains("Save").click();
+    cy.get('[data-cy="stop-title"]').should("contain.text", "Stop 1");
+
+    // check that the Tour Page also shows the new stop name
+    cy.contains("Back to Tour").click();
+    cy.get('[data-cy="tour-stop-list"]')
+      .should("contain.text", "Stop 1")
+      .should("not.contain.text", "Test Stop");
+
+    // Preview the stop should show the new stop name
+    cy.contains("Preview").click();
+    cy.get('[data-cy="stop-title"]').should("contain.text", "Stop 1");
+  });
+
   it("adds a stop subtitle");
+
   it("adds a stop image");
   it("changes the stop image");
   it("removes a stop image");

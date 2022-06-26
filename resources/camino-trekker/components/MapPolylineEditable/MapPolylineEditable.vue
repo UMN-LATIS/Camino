21c4cee266875debf6a2c0d7d442c6a494889d2a
James Johnson <johnsojr@umn.edu>
2022-06-25 19:01:20 -0500

    editable line working

--- resources/camino-trekker/components/MapPolylineEditable/MapPolylineEditable.vue
+++ resources/camino-trekker/components/MapPolylineEditable/MapPolylineEditable.vue
@@ -3,13 +3,11 @@
 </template>
 <script setup lang="ts">
 import * as MapboxDraw from "@mapbox/mapbox-gl-draw";
-import { watch, inject, onMounted, computed } from "vue";
+import { watch, inject, onMounted, computed, ref } from "vue";
 import { MapInjectionKey } from "@/shared/constants";
 import type { LngLat, TourStopRoute } from "@/types";
 import { toGeoJsonLineString } from "@/camino-trekker/components/MapPolyline/toGeoJson";
 import { fixedEndpointsMode } from "./fixedEndpointMode";
-
-const map = inject(MapInjectionKey, null);
 
 interface Props {
   startPoint: LngLat;
@@ -18,14 +16,14 @@ interface Props {
   id: string;
 }
 const props = defineProps<Props>();
-
+const isReady = ref<boolean>(false);
+const map = inject(MapInjectionKey, null);
 const draw = new MapboxDraw({
   displayControlsDefault: false,
   modes: {
     ...MapboxDraw.modes,
     fixed_endpoint: fixedEndpointsMode as unknown as MapboxDraw.DrawCustomMode,
   },
-  defaultMode: "fixed_endpoint",
 });
 
 const midpoint = computed(
@@ -39,24 +37,37 @@ const routeOrMidpoint = computed((): LngLat[] =>
   props.route.length ? props.route : [midpoint.value]
 );
 
+function renderLine() {
+  if (!isReady.value) return;
+
+  // if any line features exist, remove them
+  draw.deleteAll();
+
+  const lineFeature = toGeoJsonLineString([
+    props.startPoint,
+    ...routeOrMidpoint.value,
+    props.endPoint,
+  ]);
+  const featureIds = draw.add(lineFeature);
+  console.log({ featureIds });
+  draw.changeMode("direct_select", {
+    featureId: featureIds[0],
+  });
+}
+
 function initDrawOnMapLoad() {
   const unwatch = watch([map], () => {
     if (!map) return;
-
     map.value.addControl(draw);
-
-    const lineFeature = toGeoJsonLineString([
-      props.startPoint,
-      ...routeOrMidpoint.value,
-      props.endPoint,
-    ]);
-
-    console.log(lineFeature);
-    draw.add(lineFeature);
-    // remove this watcher
+    isReady.value = true;
     unwatch();
   });
 }
+
+watch([isReady, () => props.endPoint], () => {
+  console.log("rendering line");
+  renderLine();
+});
 
 onMounted(() => {
   initDrawOnMapLoad();
--- resources/camino-trekker/components/MapPolylineEditable/fixedEndpointMode.ts
+++ resources/camino-trekker/components/MapPolylineEditable/fixedEndpointMode.ts
@@ -6,19 +6,11 @@
  * @see https://github.com/mapbox/mapbox-gl-draw/blob/main/docs/MODES.md
  */
 
-import { MyCustomDrawMode, LngLat } from "@/types";
-import { LineString } from "geojson";
-import { toGeoJsonPoint, toGeoJsonLineString } from "../MapPolyline/toGeoJson";
+import { MyCustomDrawMode } from "@/types";
 import DirectSelectMode from "@mapbox/mapbox-gl-draw/src/modes/direct_select";
 
 export const fixedEndpointsMode: MyCustomDrawMode = {
-  onSetup(opts) {
-    console.log(opts);
-  },
-  onClick(state, event) {
-    console.log(state, event);
-  },
-
+  ...DirectSelectMode,
   toDisplayFeatures(state, geojson, display) {
     return display(geojson);
   },

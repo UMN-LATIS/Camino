be0529cf1942cde2b0c7dd3c7e61e57cc805ecfb
James Johnson <johnsojr@umn.edu>
2022-06-27 20:50:13 -0500

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
@@ -6,4 +7,10 @@ module.exports = {
     "**/resources/**/__tests__/**/*.?(m)[jt]s?(x)",
     "**/resources/**/?(*.)+(spec|test).?(m)[tj]s?(x)",
   ],
+  // set these to correspond to the paths in your tsconfig.json
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/resources/$1",
+    "@trekker/(.*)": "<rootDir>/resources/camino-trekker/$1",
+    "@creator/(.*)": "<rootDir>/resources/camino-creator/$1",
+  },
 };

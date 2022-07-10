a3757ee6a4291e692d76cde594da5c635f1ab5ef
James Johnson <johnsojr@umn.edu>
2022-07-09 19:15:32 -0500

    fix: active routes or stops may not appear to the user (#283)

---
+++ resources/camino-trekker/components/TourMap/TourMapStarMarker.vue
@@ -0,0 +1,26 @@
+<template>
+  <MapMarker :lng="lng" :lat="lat" :draggable="draggable">
+    <MapMarkerLabel :color="color" :pulse="pulse">
+      <span class="material-icons"> star </span>
+    </MapMarkerLabel>
+  </MapMarker>
+</template>
+<script setup lang="ts">
+import MapMarkerLabel from "../MapMarkerLabel/MapMarkerLabel.vue";
+import MapMarker from "../MapMarker/MapMarker.vue";
+
+withDefaults(
+  defineProps<{
+    lng: number;
+    lat: number;
+    color?: "pink" | "orange" | "default";
+    pulse?: boolean;
+    draggable?: boolean;
+  }>(),
+  {
+    color: "default",
+    pulse: false,
+    draggable: false,
+  }
+);
+</script>

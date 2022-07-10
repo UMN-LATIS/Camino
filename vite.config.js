ee33aaaa3717a0fcf9ad29c91931e942e5c3800a
James Johnson <johnsojr@umn.edu>
2022-07-09 20:23:31 -0500

    add vite config

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [
+    laravel(["resources/js/app.ts"]),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/resources",
+      "@trekker": "/resources/camino-trekker",
+      "@creator": "/resources/camino-creator",
+    },
+  },
+  server: {
+    https: true,
+  },
+});

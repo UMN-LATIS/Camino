36024416cacaa479209b3dffcfe18ca8c35018c7
James Johnson <johnsojr@umn.edu>
2022-07-13 09:53:29 -0500

    undo autoformatting

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,14 @@ Route::get('/findTours', 'HomeController@findTours');
 
 // Admin
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'permission:administer site']], function () {
-    Route::resource('users', 'Admin\\UsersController');
+  Route::resource('users', 'Admin\\UsersController');
 });
 
 // API
 Route::get('/api/tour/{tour}', "HomeController@loadTour");
 Route::get('/api/tours/', "HomeController@loadTours");
-Route::post('/feedback/{tour}', 'HomeController@storeFeedback');
 Route::post('/api/tour/{tour}/deepdivesEmail', TourDeepDivesEmailController::class);
-
-// Route::post('/emailDeepDives', 'HomeController@emailDeepDives');
+Route::post('/feedback/{tour}', 'HomeController@storeFeedback');
 
 // Camino Trekker App
 Route::get('/trekker/{all?}', [CaminoTrekkerController::class, 'index'])->where(['all' => '.*']);
@@ -42,20 +40,20 @@ Route::get('/ar/{tour}/{stage}/{locale}/{simulateLocation?}', "HomeController@ar
 
 // Camino Creator App
 Route::group(['prefix' => 'creator', 'middleware' => ['auth']], function () {
-    Route::get('/', "TourEditController@index");
-    Route::resource("edit", "TourEditController")->parameters([
-        'edit' => 'tour'
-    ]);
+  Route::get('/', "TourEditController@index");
+  Route::resource("edit", "TourEditController")->parameters([
+    'edit' => 'tour'
+  ]);
 
-    Route::post('/image/store', 'ImageController@store');
-    Route::delete('/image/{filename}', 'ImageController@delete');
-    Route::post("/edit/{tour}/stop", "TourEditController@createStop");
-    Route::put("/edit/{tour}/stop/{stop}", "TourEditController@updateStop");
-    Route::delete("/edit/{tour}/stop/{stop}", "TourEditController@deleteStop");
-    Route::get('/{tour}/feedback/', "TourEditController@getFeedback");
-    Route::post("/{tour}/share/", "TourEditController@shareTour");
-    Route::get("/{tour}/join/{tourHash}", "TourEditController@joinTour");
-    Route::any('{all}', 'TourEditController@index')->where(['all' => '.*']);
+  Route::post('/image/store', 'ImageController@store');
+  Route::delete('/image/{filename}', 'ImageController@delete');
+  Route::post("/edit/{tour}/stop", "TourEditController@createStop");
+  Route::put("/edit/{tour}/stop/{stop}", "TourEditController@updateStop");
+  Route::delete("/edit/{tour}/stop/{stop}", "TourEditController@deleteStop");
+  Route::get('/{tour}/feedback/', "TourEditController@getFeedback");
+  Route::post("/{tour}/share/", "TourEditController@shareTour");
+  Route::get("/{tour}/join/{tourHash}", "TourEditController@joinTour");
+  Route::any('{all}', 'TourEditController@index')->where(['all' => '.*']);
 });
 
 
@@ -66,18 +64,18 @@ Route::get('socialite/{provider}', 'Auth\SocialiteController@redirectToProvider'
 Route::get('socialite/{provider}/callback', 'Auth\SocialiteController@handleProviderCallback');
 
 if (config('shibboleth.emulate_idp')) {
-    Route::name('shiblogin')->get("shiblogin", '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
-    Route::group(['middleware' => 'web'], function () {
-        Route::get('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-        Route::post('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
-        Route::get('emulated/login', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
-        Route::get('emulated/logout', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
-    });
+  Route::name('shiblogin')->get("shiblogin", '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
+  Route::group(['middleware' => 'web'], function () {
+    Route::get('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+    Route::post('emulated/idp', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateIdp');
+    Route::get('emulated/login', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogin');
+    Route::get('emulated/logout', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@emulateLogout');
+  });
 } else {
-    Route::name('shiblogin')->get("shiblogin", '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@login');
-    Route::group(['middleware' => 'web'], function () {
-        Route::name('shibboleth-login')->get('/shibboleth-login', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@login');
-        Route::name('shibboleth-authenticate')->get('/shibboleth-authenticate', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@idpAuthenticate');
-        Route::name('shibboleth-logout')->get('/shibboleth-logout', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@destroy');
-    });
+  Route::name('shiblogin')->get("shiblogin", '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@login');
+  Route::group(['middleware' => 'web'], function () {
+    Route::name('shibboleth-login')->get('/shibboleth-login', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@login');
+    Route::name('shibboleth-authenticate')->get('/shibboleth-authenticate', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@idpAuthenticate');
+    Route::name('shibboleth-logout')->get('/shibboleth-logout', '\StudentAffairsUwm\Shibboleth\Controllers\ShibbolethController@destroy');
+  });
 }

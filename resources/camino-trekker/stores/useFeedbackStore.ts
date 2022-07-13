99b4b3c7fe4b05aa9e8844c33f7832ded9fff926
James Johnson <johnsojr@umn.edu>
2022-07-13 12:17:53 -0500

    add useStorage to feedback store

--- resources/camino-trekker/stores/useFeedbackStore.ts
+++ resources/camino-trekker/stores/useFeedbackStore.ts
@@ -1,16 +1,21 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
 import axios from "@/shared/axios";
 import config from "@/camino-trekker/config";
+import { useStorage } from "@vueuse/core";
+import { useTrekkerStore } from "./useTrekkerStore";
 
 export const useFeedbackStore = defineStore("feedback", {
-  state: () => ({
-    name: "",
-    email: "",
-    feedback: "",
-    isSubmitting: false,
-    isNewSubmission: true,
-    error: "",
-  }),
+  state: () => {
+    const { tourId } = useTrekkerStore();
+    return useStorage(`camino.trekker.tour-${tourId}.feedbackStore`, {
+      name: "",
+      email: "",
+      feedback: "",
+      isSubmitting: false,
+      isNewSubmission: true,
+      error: "",
+    });
+  },
   actions: {
     softReset() {
       /** keep name and email  */

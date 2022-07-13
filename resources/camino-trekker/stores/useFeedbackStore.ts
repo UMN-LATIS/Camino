422022639ec1394973d335648ef939dca228a7d9
James Johnson <johnsojr@umn.edu>
2022-07-13 13:08:32 -0500

    only persist name, email, and feedback

--- resources/camino-trekker/stores/useFeedbackStore.ts
+++ resources/camino-trekker/stores/useFeedbackStore.ts
@@ -7,14 +7,15 @@ import { useTrekkerStore } from "./useTrekkerStore";
 export const useFeedbackStore = defineStore("feedback", {
   state: () => {
     const { tourId } = useTrekkerStore();
-    return useStorage(`camino.trekker.tour-${tourId}.feedbackStore`, {
-      name: "",
-      email: "",
-      feedback: "",
+    const storageKey = `camino.trekker.tour-${tourId}.feedbackStore`;
+    return {
+      name: useStorage<string>(`${storageKey}.name`, ""),
+      email: useStorage<string>(`${storageKey}.email`, ""),
+      feedback: useStorage<string>(`${storageKey}.feedback`, ""),
       isSubmitting: false,
       isNewSubmission: true,
       error: "",
-    });
+    };
   },
   actions: {
     softReset() {

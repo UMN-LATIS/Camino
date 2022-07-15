5ce439302fcfd8edbd01d66e303892b5a4ae8de9
James Johnson <johnsojr@umn.edu>
2022-07-15 13:19:32 -0500

    initial quiz stage

---
+++ resources/camino-trekker/components/Modal/Modal.vue
@@ -0,0 +1,18 @@
+<template>
+  <dialog>
+    I'm a dialog!
+    <slot />
+    <form method="dialog">
+      <input type="submit" value="Close" />
+    </form>
+  </dialog>
+</template>
+<script setup>
+import dialogPolyfill from "dialog-polyfill";
+const dialog = document.querySelector("dialog");
+
+dialogPolyfill.registerDialog(dialog);
+</script>
+<style scoped>
+@import "dialog-polyfill/dist/dialog-polyfill.css";
+</style>

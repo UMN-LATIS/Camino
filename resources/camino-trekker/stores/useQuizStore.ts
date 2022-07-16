c926722f8b6c290e91f9234023f26ee71aecabb1
James Johnson <johnsojr@umn.edu>
2022-07-16 18:29:13 -0500

    refactor: move getQuizzesFromTour to separate file

--- resources/camino-trekker/stores/useQuizStore.ts
+++ resources/camino-trekker/stores/useQuizStore.ts
@@ -1,66 +1,27 @@
 import { defineStore, acceptHMRUpdate } from "pinia";
 import { useTrekkerStore } from "./useTrekkerStore";
-// import { useStorage } from "@vueuse/core";
-import getStagesFromStopWhere from "@/shared/getStagesFromStopWhere";
-import { QuizStage, QuizChoice, UserQuiz } from "@/types";
-
-type StopIndex = number;
-type QuizStageId = string;
-
-interface QuizStoreState {
-  /**
-   * A collection of user quizzes, their status, and responses
-   */
-  isQuizModalOpen: boolean;
-  quizzes: Record<string, UserQuiz>;
-  quizIdsByStopIndex: Record<StopIndex, QuizStageId[]>;
-}
+import { QuizChoice, UserQuiz } from "@/types";
+import { useStorage } from "@vueuse/core";
+import getQuizzesFromTour from "../utils/getQuizzesFromTour";
 
 export const useQuizStore = defineStore("quizzes", {
-  state: (): QuizStoreState => {
+  state: () => {
     const trekkerStore = useTrekkerStore();
     if (!trekkerStore.tour) {
       throw new Error("cannot initialize quiz store before tour is loaded.");
     }
 
-    const quizzes: Record<QuizStageId, UserQuiz> = {};
-    const quizIdsByStopIndex: Record<StopIndex, QuizStageId[]> = {};
-
-    trekkerStore.tour.stops.forEach((stop, index) => {
-      const stopQuizStages = getStagesFromStopWhere<QuizStage>(
-        "type",
-        "quiz",
-        stop
-      );
-
-      // if no quizzes at this stop,
-      // just add an empty array to our index of quizzes
-      // and carry on
-      if (!stopQuizStages.length) {
-        quizIdsByStopIndex[index] = [];
-        return;
-      }
-
-      // otherwise add these stages to our list
-      // with a few extra bits to make them a UserQuiz
-      stopQuizStages.forEach((stage) => {
-        quizzes[stage.id] = {
-          ...stage,
-          isComplete: false,
-          submittedResponses: [],
-          showHint: false,
-        };
-      });
-
-      // and add a list of all quizzes within this stop
-      // for stop lookup later
-      quizIdsByStopIndex[index] = stopQuizStages.map((s) => s.id);
-    });
+    const { quizzes, quizIdsByStopIndex } = getQuizzesFromTour(
+      trekkerStore.tour
+    );
 
     return {
       isQuizModalOpen: false,
-      quizzes,
-      quizIdsByStopIndex,
+      quizzes: useStorage("camino.trekker.quizStore.quizzes", quizzes),
+      quizIdsByStopIndex: useStorage(
+        "camino.trekker.quizStore.quizIdsByStopIndex",
+        quizIdsByStopIndex
+      ),
     };
   },
   getters: {
@@ -80,10 +41,6 @@ export const useQuizStore = defineStore("quizzes", {
       this.quizzes[quizStageId].isComplete = true;
     },
     submitQuizResponse(quizStageId: string, response: QuizChoice) {
-      // for now, we just check the response object itself it's correct
-      // since this is low stakes and all client-side anyway
-      // if things becomes higher stakes (aka gradebook integration)
-      // then we should do an api call
       if (response.correct) {
         this.markQuizComplete(quizStageId);
       }

da510b78b1708208a58cce4db88fbf14866c6c2e
James Johnson <johnsojr@umn.edu>
2022-07-16 19:09:57 -0500

    add simple settings page to reset local storage

--- resources/camino-trekker/pages/SettingsPage.vue
+++ resources/camino-trekker/pages/SettingsPage.vue
@@ -1,4 +1,43 @@
 <template>
-  <h1>Settings Page</h1>
+  <div class="settings-page container">
+    <XButton class="close-button" @click="$router.back()">Close</XButton>
+
+    <h1>Settings</h1>
+
+    <h2>Reset Local Storage</h2>
+    <p>Reset all tours and quizzes stored within your browser.</p>
+    <Button icon="refresh" @click="handleStorageReset">Reset</Button>
+  </div>
 </template>
-<script setup></script>
+<script setup lang="ts">
+import Button from "../components/Button/Button.vue";
+import XButton from "../components/XButton/XButton.vue";
+
+function handleStorageReset() {
+  if (confirm("Reset Tour and Quizzes?")) {
+    window.localStorage.clear();
+  }
+}
+</script>
+<style scoped>
+.settings-page {
+  padding: 2rem;
+  background: #f3f3f3;
+  min-height: 100vh;
+}
+.close-button {
+  position: absolute;
+  top: 2rem;
+  right: 2rem;
+}
+
+h1 {
+  border-bottom: 1px solid var(--black);
+  padding-bottom: 1rem;
+}
+
+h2 {
+  font-size: 1rem;
+  text-transform: uppercase;
+}
+</style>

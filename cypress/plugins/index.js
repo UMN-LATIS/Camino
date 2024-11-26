cdfb486d1c3de87bda9f30c99e9367d9e8503e8f
James Johnson <johnsojr@umn.edu>
2024-11-26 17:27:02 -0600

    esmify cypress plugins

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,7 @@
  */
 // eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-module.exports = (on, config) => {
+export default (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };

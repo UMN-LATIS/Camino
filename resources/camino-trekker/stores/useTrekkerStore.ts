5aacf55dc353cb266d7cbcaa1560b873b4626a0d
James Johnson <johnsojr@umn.edu>
2025-05-01 14:57:42 -0700

    fix: permit trekker store to `useRoute`
    Pinia docs say to use composition api for this, rather than options.

--- resources/camino-trekker/stores/useTrekkerStore.ts
+++ resources/camino-trekker/stores/useTrekkerStore.ts
@@ -1,95 +1,95 @@
-import { defineStore, acceptHMRUpdate } from "pinia";
+import { defineStore } from "pinia";
 import { toursService } from "../common/api.service";
-import { Maybe, Tour, TourStop, Locale, BottomNavSheet } from "@/types";
+import { Maybe, Tour, Locale, BottomNavSheet } from "@/types";
 import { useRoute } from "vue-router";
 import normalizeTour from "@/shared/normalizeTour";
 import { useStorage } from "@vueuse/core";
+import { reactive, computed, toRefs } from "vue";
 
 const toInt = (str) => Number.parseInt(str, 10);
 
-export const useTrekkerStore = defineStore("trekker", {
-  state: () => {
-    const route = useRoute();
+export const useTrekkerStore = defineStore("trekker", () => {
+  const route = useRoute();
+  const storageKey = `camino.trekker.tour-${route.params.tourId}.trekkerStore`;
 
-    const storageKey = `camino.trekker.tour-${route.params.tourId}.trekkerStore`;
+  const state = reactive({
+    tour: null as Maybe<Tour>,
+    isLoading: true,
+    locale: useStorage<Locale>(`${storageKey}.locale`, Locale.en),
+    errors: [] as string[],
+    activeSheet: null as Maybe<BottomNavSheet>,
+  });
 
-    return {
-      tour: null as Maybe<Tour>,
-      isLoading: true,
-      locale: useStorage<Locale>(`${storageKey}.locale`, Locale.en),
-      errors: [] as string[],
-      activeSheet: null as Maybe<BottomNavSheet>,
-    };
-  },
-  getters: {
-    allStops: (state) => state.tour?.stops ?? [],
-    totalStops: (state) => state.tour?.stops.length ?? 0,
-    stopIndex(): number {
-      const route = useRoute();
-      const stopIndex = Array.isArray(route.params.stopIndex)
-        ? route.params.stopIndex[0]
-        : route.params.stopIndex;
-      return Number.parseInt(stopIndex) ?? 0;
-    },
-    tourId(): number {
-      const route = useRoute();
-      return toInt(route.params.tourId);
-    },
-    isFirstStop(): boolean {
-      return this.stopIndex === 0;
-    },
-    isLastStop(): boolean {
-      return this.stopIndex === this.totalStops - 1;
-    },
-    currentStop(): TourStop {
-      return this.allStops[this.stopIndex];
-    },
-    nextStop(): Maybe<TourStop> {
-      if (this.isLastStop) return null;
-      return this.allStops[this.stopIndex + 1];
-    },
-    previousStop(): Maybe<TourStop> {
-      if (this.isFirstStop) return null;
-      return this.allStops[this.stopIndex - 1];
-    },
-    supportedLocales(state): Locale[] {
-      return state.tour?.tour_content?.languages ?? [];
-    },
-    isActiveSheet:
-      (state) =>
-      (sheetKey: Maybe<BottomNavSheet>): boolean =>
-        state.activeSheet === sheetKey,
-  },
-  actions: {
+  const allStops = computed(() => state.tour?.stops ?? []);
+  const totalStops = computed(() => state.tour?.stops.length ?? 0);
+  const stopIndex = computed(() => {
+    const stopIndex = Array.isArray(route.params.stopIndex)
+      ? route.params.stopIndex[0]
+      : route.params.stopIndex;
+    return Number.parseInt(stopIndex) ?? 0;
+  });
+
+  const tourId = computed(() => {
+    return toInt(route.params.tourId);
+  });
+  const isFirstStop = computed(() => stopIndex.value === 0);
+  const isLastStop = computed(() => stopIndex.value === totalStops.value - 1);
+  const currentStop = computed(() => allStops.value[stopIndex.value]);
+  const nextStop = computed(() => {
+    if (isLastStop.value) return null;
+    return allStops.value[stopIndex.value + 1];
+  });
+  const previousStop = computed(() => {
+    if (isFirstStop.value) return null;
+    return allStops.value[stopIndex.value - 1];
+  });
+  const supportedLocales = computed(
+    () => state.tour?.tour_content?.languages ?? []
+  );
+
+  const isActiveSheet = (sheetKey: Maybe<BottomNavSheet>): boolean => {
+    return state.activeSheet === sheetKey;
+  };
+
+  const actions = {
     fetchTour(tourId) {
-      this.isLoading = true;
+      state.isLoading = true;
       toursService
         .get(tourId)
         .then((tour) => {
-          this.isLoading = false;
-          this.tour = normalizeTour(tour);
+          state.isLoading = false;
+          state.tour = normalizeTour(tour);
         })
         .catch((err) => {
           console.error(err);
-          this.isLoading = false;
-          this.errors.push(err);
+          state.isLoading = false;
+          state.errors.push(err);
         });
     },
     setLocale(locale: Locale) {
-      this.locale = locale;
+      state.locale = locale;
     },
     setActiveSheet(sheetKey: Maybe<BottomNavSheet>) {
-      this.activeSheet = sheetKey;
+      state.activeSheet = sheetKey;
     },
     closeActiveSheet() {
-      this.setActiveSheet(null);
+      actions.setActiveSheet(null);
     },
-  },
-});
+  };
 
-// see: https://pinia.vuejs.org/cookbook/hot-module-replacement.html
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept(
-    acceptHMRUpdate(useTrekkerStore, import.meta.webpackHot)
-  );
-}
+  return {
+    ...toRefs(state),
+    ...actions,
+    allStops,
+    totalStops,
+    stopIndex,
+    tourId,
+    isFirstStop,
+    isLastStop,
+    currentStop,
+    nextStop,
+    previousStop,
+    supportedLocales,
+    isActiveSheet,
+  };
+});

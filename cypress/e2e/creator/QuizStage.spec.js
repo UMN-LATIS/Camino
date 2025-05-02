b3f4f3e1b3c881b693744e2cc64a94b0d99dbe03
James Johnson <johnsojr@umn.edu>
2025-05-01 23:01:24 -0700

    add sleep to wait for typing to finish?

--- cypress/e2e/creator/QuizStage.spec.js
+++ cypress/e2e/creator/QuizStage.spec.js
@@ -35,6 +35,9 @@ describe("Tour Stop Page", () => {
     cy.get('[data-cy="quiz-prompt-editor"] .ql-editor').type(
       "What's the best color?"
     );
+
+    cy.sleep(500); // wait for typing to finish
+
     cy.get('[data-cy="quiz-prompt-editor"]').should(
       "contain.text",
       "What's the best color?"

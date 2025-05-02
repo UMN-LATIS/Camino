087e023f491da911a7659a4a19f76ae9a0afca52
James Johnson <johnsojr@umn.edu>
2025-05-01 22:21:56 -0700

    fix: add a quiz stage test

--- cypress/e2e/creator/QuizStage.spec.js
+++ cypress/e2e/creator/QuizStage.spec.js
@@ -32,7 +32,9 @@ describe("Tour Stop Page", () => {
       "quiz"
     );
 
-    cy.get('[data-cy="quiz-prompt-editor"]').type("What's the best color?");
+    cy.get('[data-cy="quiz-prompt-editor"] .ql-editor').type(
+      "What's the best color?"
+    );
     cy.get('[data-cy="quiz-prompt-editor"]').should(
       "contain.text",
       "What's the best color?"

c1ef05bd72e28fc45079727fc50ef8ba20179b7d
James Johnson <johnsojr@umn.edu>
2026-05-14 21:24:22 -0500

    chore: upgrade to Laravel 13 and related dependencies
    - laravel/framework ^13.0
    - intervention/image v2 → intervention/image-laravel v1 (image v3)
    - barryvdh/laravel-debugbar ^4.0
    - laravel/tinker ^3.0, laravel/boost ^2.0
    - spatie/laravel-permission ^7.0
    - symfony/http-client ^7.4, symfony/mailgun-mailer ^7.4
    - toin0u/geocoder-laravel ^13.0
    - laracasts/cypress ^3.0.4
    - Fix PDO::MYSQL_ATTR_SSL_CA → Pdo\Mysql::ATTR_SSL_CA for PHP 8.5
    - Migrate phpunit.xml to current schema
    - Add .phpunit.cache/ to .gitignore

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -1,43 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\FileUpload;
 use Illuminate\Http\Request;
 use Validator;
 use Illuminate\Support\Str;
 use Storage;
 use App\Tour;
+use Intervention\Image\Laravel\Facades\Image;
 
-class ImageController extends Controller
-{
-    public function store(Request $request)
-    {
+class ImageController extends Controller {
+    public function store(Request $request) {
         $this->authorize('create', Tour::class);
-        if($request->file('image'))
-        {
+        if ($request->file('image')) {
             $validator = Validator::make($request->all(), [
-                 'image'  => 'required|max:8192',
+                'image'  => 'required|max:8192',
             ]);
             if ($validator->fails()) {
                 return response()->json(['error' => $validator->errors()->getMessages()], 500);
             }
             $image = $request->file('image');
-            \Image::configure(array('driver' => 'imagick'));
-            $image_resized = \Image::make($image)->orientate();
-            $image_resized->resize(2048, 2048, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            });
+            $image_resized = Image::read($image)
+                ->orientate()
+                ->scaleDown(2048, 2048);
 
             $path =  "public/" . Str::random(40) . ".jpg";
-            Storage::put($path, (string) $image_resized->encode("jpg", 70));
+            Storage::put($path, $image_resized->toJpeg(70));
             $imagePath = Storage::url($path);
             // $imagePath = $image->store('public');
-            return response()->json(['success' => 'You have successfully uploaded an image', 'image'=>basename($imagePath)], 200);
+            return response()->json(['success' => 'You have successfully uploaded an image', 'image' => basename($imagePath)], 200);
         }
-        
-        
-        
     }
 
     public function delete(Request $request, $filename) {

89f8c9d11923b9990f9f1fa517a6865200a27f19
James Johnson <johnsojr@umn.edu>
2026-05-19 13:36:37 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import laravel from "laravel-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+  const appUrl = env.VITE_APP_URL ? new URL(env.VITE_APP_URL) : null;
 
   return {
     plugins: [
@@ -72,7 +73,7 @@ export default defineConfig(({ mode }) => {
     },
 
     server: {
-      host: env.VITE_APP_URL ? new URL(env.VITE_APP_URL).host : "127.0.0.1",
+      host: appUrl ? appUrl.hostname : "127.0.0.1",
       https: {
         cert: "./.cert/cert.pem",
         key: "./.cert/key.pem",
